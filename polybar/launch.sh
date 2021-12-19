8de6ea1d9375687275aa05041af2b481f75474c3
Ash (ashisbored) <ash.b.7558@gmail.com>
2021-12-19 22:16:55 +0000

    polybar: Move to a new bar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -13,6 +13,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload media &
+    MONITOR=$m polybar --reload info &
   done
 else
   polybar --reload media &
